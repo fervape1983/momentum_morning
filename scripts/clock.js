f2090347fe5b01f1744abe85da8c7aad72adea83
fervape1983 <54364915+fervape1983@users.noreply.github.com>
2019-08-26 12:07:35 -0400

    complete clock
    :)

--- scripts/clock.js
+++ scripts/clock.js
@@ -4,10 +4,12 @@ const clockTitle = clockContainer.querySelector("h1");
 function getTime() {
   const date = new Date();
   const hours = date.getHours();
-  const minutes = Date.getMinutes();
+  const minutes = date.getMinutes();
   const seconds = date.getSeconds();
 
-  clockTitle.innerText = `${hours} : ${minutes} : ${seconds}`;
+  clockTitle.innerText = `${hours < 10 ? `0${hours}` : `${hours}`} : ${
+    minutes < 10 ? `0${minutes}` : `${minutes}`
+  } : ${seconds < 10 ? `0${seconds}` : `${seconds}`}`;
 }
 
 function init() {
